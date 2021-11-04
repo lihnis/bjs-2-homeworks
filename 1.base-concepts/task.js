1eb2761d4eb23605e30af2bff97a4b23741c8c22
Natalya Grom <nata_grom@mail.ru>
2021-11-04 13:58:04 +0300

    Задача 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,25 @@
+"use strict";
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  if (a == 0) {
+     return "Ошибка, а = 0 недопустимое значение";
+  }
+  
+  let D = Math.pow(b,2) - (4*a*c);
+  
+  if (D < 0) {
+     return arr; 
+  }
+
+ else if (D == 0) {
+    arr[0] = -b / (2 * a);
+ }
+
+  else if (D > 0) {
+    arr[0] = -b + Math.sqrt(D)/ (2 * a);
+    arr[1] = -b - Math.sqrt(D)/ (2 * a);
+    }
+
   return arr; // array
 }
 
